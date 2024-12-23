4d63eca8771d932a0d9514370fc0436ca2d45227
MD FAHIM MUNTASHIR <taslimit23@gmail.com>
2024-12-23 14:45:36 +0630

    sdf

--- frontend/src/Pages/Group/Agro/Partial/Welcome.jsx
+++ frontend/src/Pages/Group/Agro/Partial/Welcome.jsx
@@ -11,7 +11,7 @@ const Welcome = () => {
         />
       </div>
       <div className="p-2 rounded-lg lg:w-1/2">
-        <h1 className="text-xl lg:text-5xl font_title font-bold mb-4 border-b-2 max-w-fit text-color_3">
+        <h1 className="text-xl lg:text-5xli font_title font-bold mb-4 border-b-2 max-w-fit text-color_3">
           Welcome to Saayan Agro
         </h1>
         <p className="text-lg font-medium text-justify">
